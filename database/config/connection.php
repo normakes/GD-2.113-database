3a60d555cbe34a436a9b967af1708a40ac18ff65
normakes <38392344+normakes@users.noreply.github.com>
2018-06-25 16:06:25 -0500

    Initial commit

---
+++ database/config/connection.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "127.0.0.1";
+$username = "11";
+$password = "";
+$dbname = "11";
+?>
---
+++ database/connection.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+$servername = "localhost";
+$username = "11";
+$password = "";
+$dbname = "11";
+
+try {
+    $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, array(
+    PDO::ATTR_PERSISTENT => true
+));
+    // set the PDO error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
